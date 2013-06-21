1240d5e503f05e781559e7d6f27d54f6671f2113
Justin Brantley <justinbrantley@me.com>
2013-06-20 23:50:59 -0700

    Beef up controller spec; tidy code

--- spec/controllers/pulls_controller_spec.rb
+++ spec/controllers/pulls_controller_spec.rb
@@ -9,119 +9,170 @@ describe PullsController do
 
   let(:valid_attributes) { FactoryGirl.attributes_for(:pull) }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # PullsController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
   describe "GET index" do
+    it 'responds successfully with an HTTP 200 status code' do
+      get :index
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
     it "assigns all pulls as @pulls" do
       pull = Pull.create! valid_attributes
       get :index, {}, valid_session
-      assigns(:pulls).should eq([pull])
+      expect(assigns(:pulls)).to eq([pull])
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
     end
   end
 
   describe "GET show" do
-    it "assigns the requested pull as @pull" do
+    before(:each) do
       pull = Pull.create! valid_attributes
-      get :show, {:id => pull.to_param}, valid_session
-      assigns(:pull).should eq(pull)
+      get :show, {id: pull.to_param}, valid_session
+    end
+
+    it 'responds successfully with an HTTP 200 status code' do
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it "assigns the requested pull as @pull" do
+      expect(assigns(:pull)).to eq(Pull.last)
+    end
+
+    it "renders the show template" do
+      expect(response).to render_template("show")
     end
   end
 
   describe "GET new" do
+    before(:each) { get :new, {}, valid_session }
+
+    it 'responds successfully with an HTTP 200 status code' do
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
     it "assigns a new pull as @pull" do
-      get :new, {}, valid_session
-      assigns(:pull).should be_a_new(Pull)
+      expect(assigns(:pull)).to be_a_new(Pull)
+    end
+
+    it "renders the new template" do
+      expect(response).to render_template("new")
     end
   end
 
   describe "GET edit" do
-    it "assigns the requested pull as @pull" do
+    before(:each) do
       pull = Pull.create! valid_attributes
-      get :edit, {:id => pull.to_param}, valid_session
-      assigns(:pull).should eq(pull)
+      get :edit, {id: pull.to_param}, valid_session
+    end
+
+    it 'responds successfully with an HTTP 200 status code' do
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it "assigns the requested pull as @pull" do
+      expect(assigns(:pull)).to eq(Pull.last)
+    end
+
+    it "renders the edit template" do
+      expect(response).to render_template("edit")
     end
   end
 
   describe "POST create" do
-    describe "with valid params" do
+    context "with valid params" do
       it "creates a new Pull" do
         expect {
-          post :create, {:pull => valid_attributes}, valid_session
+          post :create, {pull: valid_attributes}, valid_session
         }.to change(Pull, :count).by(1)
       end
 
+      it "displays a notice of success" do
+        post :create, {pull: valid_attributes}, valid_session
+        expect(flash[:notice]).to match /successfully created/
+      end
+
       it "assigns a newly created pull as @pull" do
-        post :create, {:pull => valid_attributes}, valid_session
-        assigns(:pull).should be_a(Pull)
-        assigns(:pull).should be_persisted
+        post :create, {pull: valid_attributes}, valid_session
+        expect(assigns(:pull)).to be_a(Pull)
+        expect(assigns(:pull)).to be_persisted
       end
 
       it "redirects to the created pull" do
-        post :create, {:pull => valid_attributes}, valid_session
-        response.should redirect_to(Pull.last)
+        post :create, {pull: valid_attributes}, valid_session
+        expect(response).to redirect_to(Pull.last)
       end
     end
 
-    describe "with invalid params" do
+    context "with invalid params" do
+      it "does not create a new Pull" do
+        expect {
+          post :create, {pull: { name: "" }}, valid_session
+        }.to change(Pull, :count).by(0)
+      end
+
       it "assigns a newly created but unsaved pull as @pull" do
-        # Trigger the behavior that occurs when invalid params are submitted
         Pull.any_instance.stub(:save).and_return(false)
-        post :create, {:pull => { "name" => "invalid value" }}, valid_session
-        assigns(:pull).should be_a_new(Pull)
+        post :create, {pull: { name: "" }}, valid_session
+        expect(assigns(:pull)).to be_a_new(Pull)
       end
 
       it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
         Pull.any_instance.stub(:save).and_return(false)
-        post :create, {:pull => { "name" => "invalid value" }}, valid_session
-        response.should render_template("new")
+        post :create, {pull: { name: "" }}, valid_session
+        expect(response).to render_template("new")
       end
     end
   end
 
   describe "PUT update" do
-    describe "with valid params" do
+    context "with valid params" do
       it "updates the requested pull" do
         pull = Pull.create! valid_attributes
-        # Assuming there are no other pulls in the database, this
-        # specifies that the Pull created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
         Pull.any_instance.should_receive(:update).with({ "name" => "MyString" })
-        put :update, {:id => pull.to_param, :pull => { "name" => "MyString" }}, valid_session
+        put :update, {id: pull.to_param, pull: { "name" => "MyString" }}, valid_session
+      end
+
+      it "displays a notice of success" do
+        pull = Pull.create! valid_attributes
+        put :update, {id: pull.to_param, pull: valid_attributes}, valid_session
+        expect(flash[:notice]).to match /successfully updated/
       end
 
       it "assigns the requested pull as @pull" do
         pull = Pull.create! valid_attributes
-        put :update, {:id => pull.to_param, :pull => valid_attributes}, valid_session
-        assigns(:pull).should eq(pull)
+        put :update, {id: pull.to_param, pull: valid_attributes}, valid_session
+        expect(assigns(:pull)).to eq(pull)
       end
 
       it "redirects to the pull" do
         pull = Pull.create! valid_attributes
-        put :update, {:id => pull.to_param, :pull => valid_attributes}, valid_session
-        response.should redirect_to(pull)
+        put :update, {id: pull.to_param, pull: valid_attributes}, valid_session
+        expect(response).to redirect_to(pull)
       end
     end
 
-    describe "with invalid params" do
+    context "with invalid params" do
       it "assigns the pull as @pull" do
         pull = Pull.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
         Pull.any_instance.stub(:save).and_return(false)
-        put :update, {:id => pull.to_param, :pull => { "name" => "invalid value" }}, valid_session
-        assigns(:pull).should eq(pull)
+        put :update, {id: pull.to_param, pull: { name: "" }}, valid_session
+        expect(assigns(:pull)).to eq(pull)
       end
 
       it "re-renders the 'edit' template" do
         pull = Pull.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
         Pull.any_instance.stub(:save).and_return(false)
-        put :update, {:id => pull.to_param, :pull => { "name" => "invalid value" }}, valid_session
-        response.should render_template("edit")
+        put :update, {id: pull.to_param, pull: { name: "" }}, valid_session
+        expect(response).to render_template("edit")
       end
     end
   end
@@ -130,14 +181,20 @@ describe PullsController do
     it "destroys the requested pull" do
       pull = Pull.create! valid_attributes
       expect {
-        delete :destroy, {:id => pull.to_param}, valid_session
+        delete :destroy, {id: pull.to_param}, valid_session
       }.to change(Pull, :count).by(-1)
+    end
+
+    it "displays a notice of success" do
+      pull = Pull.create! valid_attributes
+      delete :destroy, {id: pull.to_param}, valid_session
+      expect(flash[:notice]).to match /successfully deleted/
     end
 
     it "redirects to the pulls list" do
       pull = Pull.create! valid_attributes
-      delete :destroy, {:id => pull.to_param}, valid_session
-      response.should redirect_to(pulls_url)
+      delete :destroy, {id: pull.to_param}, valid_session
+      expect(response).to redirect_to(pulls_url)
     end
   end
 
