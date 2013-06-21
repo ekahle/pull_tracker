4d0a78c036ab5bc024303898fa1da3f336d992ff
Justin Brantley <justinbrantley@me.com>
2013-06-20 21:19:28 -0700

    Use predicate matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 
 describe User do
   it 'must have a valid factory' do
-    expect(FactoryGirl.build(:user).valid?).to be_true
+    expect(FactoryGirl.build(:user)).to be_valid
     expect(FactoryGirl.create(:user)).to be_true
     expect(FactoryGirl.create(:user)).to be_true
   end
 
   it 'must have a first and last name' do
-    expect(FactoryGirl.build(:user, first_name: '').valid?).to be_false
-    expect(FactoryGirl.build(:user, last_name: '').valid?).to be_false
+    expect(FactoryGirl.build(:user, first_name: '')).to be_invalid
+    expect(FactoryGirl.build(:user, last_name: '')).to be_invalid
   end
 
   it 'must have an email address that looks like an email address' do
-    expect(FactoryGirl.build(:user, email: 'foo').valid?).to be_false
-    expect(FactoryGirl.build(:user, email: 'foo@bar').valid?).to be_false
-    expect(FactoryGirl.build(:user, email: 'foo.bar').valid?).to be_false
-    expect(FactoryGirl.build(:user, email: 'foo@bar.baz').valid?).to be_true
+    expect(FactoryGirl.build(:user, email: 'foo')).to be_invalid
+    expect(FactoryGirl.build(:user, email: 'foo@bar')).to be_invalid
+    expect(FactoryGirl.build(:user, email: 'foo.bar')).to be_invalid
+    expect(FactoryGirl.build(:user, email: 'foo@bar.baz')).to be_valid
   end
 
   it 'must have a unique email address' do
@@ -25,8 +25,8 @@ describe User do
   end
 
   it 'must have a password that is at least eight characters long' do
-    expect(FactoryGirl.build(:user, password: 'pw').valid?).to be_false
-    expect(FactoryGirl.build(:user, password: 'password').valid?).to be_true
+    expect(FactoryGirl.build(:user, password: 'pw')).to be_invalid
+    expect(FactoryGirl.build(:user, password: 'password')).to be_valid
   end
 
   it 'should concatenate first and last names into the full name' do
