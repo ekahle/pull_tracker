4f087275a9902426ecb25ed6d1edae3fe751428f
Justin Brantley <brantley@uw.edu>
2013-06-20 17:52:29 -0700

    Validate name

--- spec/models/pull_spec.rb
+++ spec/models/pull_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Pull do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'must have a valid factory' do
+    expect(FactoryGirl.build(:pull)).to be_valid
+  end
+
+  it 'must have a name' do
+    expect(FactoryGirl.build(:pull, name: nil)).to be_invalid
+    expect(FactoryGirl.build(:pull, name: '')).to be_invalid
+  end
+
+  it 'must have a unique name' do
+    pull_params = FactoryGirl.attributes_for(:pull)
+    Pull.create! pull_params
+    expect(FactoryGirl.build(:pull, pull_params)).to be_invalid
+  end
 end
