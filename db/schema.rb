d41de580941ea7bee349ec0023d936f2f2bf8246
Erin Kahle <ekahle@uw.edu>
2013-10-15 14:14:03 -0700

    Migrated new Pull List

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131015202126) do
+ActiveRecord::Schema.define(version: 20131015211222) do
+
+  create_table "pull_lists", force: true do |t|
+    t.string   "file"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "pulls", force: true do |t|
     t.string  "status"
