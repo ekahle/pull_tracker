d994ba94afff63fc83dfa1e067ae7dd6ff38b66d
Erin Kahle <ekahle@uw.edu>
2013-10-17 10:11:28 -0700

    Added has_many belongs_to for lists and manifests Started working on front page summary

--- app/models/pull.rb
+++ app/models/pull.rb
@@ -1,5 +1,6 @@
 class Pull < ActiveRecord::Base
   has_many :pull_lists
+  has_many :pull_manifests
   #validates :pull_number, :name, presence: true, uniqueness: true
   validates :status, :description, :specimen_type, presence: true
 end
