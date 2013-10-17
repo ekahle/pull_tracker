23cbd9e827502413da3eda97a148e2e74d85b344
Erin Kahle <ekahle@uw.edu>
2013-10-17 11:29:15 -0700

    Updated home page with summary of pulls

--- app/models/pull.rb
+++ app/models/pull.rb
@@ -1,6 +1,7 @@
 class Pull < ActiveRecord::Base
   has_many :pull_lists
   has_many :pull_manifests
+  default_scope order('id DESC')
   #validates :pull_number, :name, presence: true, uniqueness: true
   validates :status, :description, :specimen_type, presence: true
 end
